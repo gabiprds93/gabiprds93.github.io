b3fe230d9d4d41896b46a3d40a2ef902e2543e11
Gabriela Paredes <gabiprds.93@gmail.com>
2024-04-26 19:01:46 -0500

    Add information of experience. Refactor icon and constants. Improve styles

---
+++ src/constants.js
@@ -0,0 +1,56 @@
+export const EXPERIENCE = [
+  {
+    date: 'Nov. 2021 - Nov. 2023',
+    title: 'Frontend Developer',
+    company: 'Terapify',
+    description: [
+      'Implementar el nuevo diseño al espacio de los usuarios que tiene sesión iniciada.',
+      'Construcción de una calendario para el registro diario de como se sienten los usuarios.',
+      'Documentación e implementación de pautas para estandarizar el desarrollo entre los integrantes del área de tecnología.',
+    ],
+    link: 'https://www.terapify.com/',
+  },
+  {
+    date: 'Oct. 2020 - Oct. 2021',
+    title: 'Frontend Engineer',
+    company: 'Trade EC',
+    description: [
+      'Maquetación y programación de SPA y App móvil.',
+      'Resolución de conflictos en Bitbucket sobre el Frontend.',
+      'Reducir el tiempo de revisión del código en un 5% desarrollando código de calidad.',
+      'Reducir el 50% del tiempo en una tarea implementando el react-select que tenía las funcionalidades que se requerían.',
+      'Sugerir cambios en los estilos para una mejor experiencia de usuario.',
+    ],
+    link: 'https://trdglobal.io/',
+  },
+  {
+    date: 'Jun. 2019 - Jun. 2020',
+    title: 'Software Engineer, Frontend',
+    company: 'Urbaner',
+    description: [
+      'Maquetación y programación de SPA.',
+      'Resolución de conflictos en Gitlab sobre el Frontend.',
+      'Implementar el nuevo diseño de creación de pedidos mejorando la experiencia del usuario.',
+      'Integrar la nueva apariencia de la web hecha en Jquery hacia ReactJs.',
+      'Participar en reuniones semanales dedicadas a temas de tecnología.',
+      'Implementar funcionalidad de geolocalización, reduciendo costos para la empresa.',
+    ],
+    link: 'https://www.urbaner.com/',
+  },
+  {
+    date: 'Ago. 2018 - May. 2019',
+    title: 'Analista Programador Frontend',
+    company: 'TECNIKA SOLUCIONES S.R.L.',
+    description: ['Maquetación y programación de Landing Page.'],
+  },
+  {
+    date: 'Abr. 2018 - Jul. 2018',
+    title: 'Frontend Developer',
+    company: 'Eureka Tech',
+    description: [
+      'Maquetación y programación de Landing Page y One Page.',
+      'Maquetación de Mailing para envíos masivos a través de EmBlue.',
+      'Resolución de conflictos en Gitlab sobre el Frontend.',
+    ],
+  },
+]
